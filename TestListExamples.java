376c63b0cabc70cc5f86baf581f4cb432e129750
OasisAlex <114462126+OasisAlex@users.noreply.github.com>
2022-11-10 17:27:23 -0800

    fix

--- TestListExamples.java
+++ TestListExamples.java
@@ -2,5 +2,27 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class TestListExamples {
-  // Write your grading tests here!
+  static List<String> merge(List<String> list1, List<String> list2) {
+    List<String> result = new ArrayList<>();
+    int index1 = 0, index2 = 0;
+    while(index1 < list1.size() && index2 < list2.size()) {
+      if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
+        result.add(list1.get(index1));
+        index1 += 1;
+      }
+      else {
+        result.add(list2.get(index2));
+        index2 += 1;
+      }
+    }
+    while(index1 < list1.size()) {
+      result.add(list1.get(index1));
+      index1 += 1;
+    }
+    while(index2 < list2.size()) {
+      result.add(list2.get(index2));
+      index1 += 1;
+    }
+    return result;
+  }
 }
--- grade.sh
+++ grade.sh
@@ -5,3 +5,13 @@ set -e
 rm -rf student-submission
 git clone $1 student-submission
 
+echo "successfully copy"
+cd student-submission
+
+if [-e ListExample.java];
+then
+    echo "The file exist"
+else
+    echo "The file not found"
+    exit
+fi
