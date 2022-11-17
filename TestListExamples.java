efcac99856984592d79aae5b40133af40789e092
OasisAlex <114462126+OasisAlex@users.noreply.github.com>
2022-11-17 12:22:33 -0800

    update

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,28 +1,90 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.zip.CheckedInputStream;
 
-public class TestListExamples {
-  static List<String> merge(List<String> list1, List<String> list2) {
+
+class listfiltercheck implements StringChecker {
+  String str;
+  public listfiltercheck() { str = " "; }
+  public listfiltercheck(String s) { str = s; }
+  public boolean checkString(String s)
+    {
+        return s.contains(str);
+    }
+}
+
+public class TestListExamples{
+    @Test
+    public void filtercheck() { 
+        listfiltercheck sc = new listfiltercheck("app");
+        List<String> list1 = new ArrayList<>();
+        list1.add("apple");
+        list1.add("whole");
+        list1.add("app");
+
+        List<String> result = new ArrayList<>();
+        result.add("apple");
+        result.add("app");
+        assertEquals(result, ListExamples.filter(list1,sc));
+    }
+
+    @Test
+    public void filtercheck1() { 
+        listfiltercheck sc = new listfiltercheck();
+        List<String> list1 = new ArrayList<>();
+        list1.add("g");
+        list1.add("1");
+        list1.add("g");
+        list1.add("s");
+
+        List<String> result = new ArrayList<>();
+        list1.add("g");
+        list1.add("1");
+        list1.add("g");
+        list1.add("s");
+
+      
+        assertEquals(result, ListExamples.filter(list1,sc));
+    }
+
+    @Test 
+	public void testFilter1() {
+        List<String> list1 = new ArrayList<>();
+        list1.add("a");
+        list1.add("b");
+        list1.add("g");
+
+        List<String> list2 = new ArrayList<>();
+        list2.add("c");
+        list2.add("f");
+        list2.add("w");
+        List<String> result = new ArrayList<>();
+        result.add("a");
+        result.add("b");
+        result.add("c");
+        result.add("f");
+        result.add("g");
+        result.add("w");
+        assertEquals(result, ListExamples.merge(list1, list2));
+	}
+
+    @Test 
+	public void testFilter2() {
+    List<String> list1 = new ArrayList<>();
+    list1.add("c");
+    List<String> list2 = new ArrayList<>();
+    list2.add("a");
     List<String> result = new ArrayList<>();
-    int index1 = 0, index2 = 0;
-    while(index1 < list1.size() && index2 < list2.size()) {
-      if(list1.get(index1).compareTo(list2.get(index2)) < 0) {
-        result.add(list1.get(index1));
-        index1 += 1;
-      }
-      else {
-        result.add(list2.get(index2));
-        index2 += 1;
-      }
+    result.add("a");
+    result.add("c");
+    assertEquals(result, ListExamples.merge(list1, list2));
+	}
+    @Test(timeout = 100)
+    public void testTimeout() {
+	while(true) {}
     }
-    while(index1 < list1.size()) {
-      result.add(list1.get(index1));
-      index1 += 1;
-    }
-    while(index2 < list2.size()) {
-      result.add(list2.get(index2));
-      index1 += 1;
-    }
-    return result;
-  }
+
 }
--- grade.sh
+++ grade.sh
@@ -1,17 +1,62 @@
 # Create your grading script here
-
 set -e
-
 rm -rf student-submission
 git clone $1 student-submission
 
 echo "successfully copy"
 cd student-submission
 
-if [-e ListExamples.java];
+if [[ -e ListExamples.java ]]
 then
     echo "The file exist"
 else
     echo "The file not found"
-    exit
+    echo "Maybe Wrong File Name"
+    exit 1
 fi
+
+
+cd ../
+cp -rf lib student-submission
+cp -f TestListExamples.java student-submission
+
+cd student-submission
+
+ls 
+if [[ -e TestListExamples.java ]]
+then
+   echo "The Testlistexamples file is been copy"
+else
+   echo "The Testlistexamples file is not been copy"
+   exit 1
+fi
+
+cpath=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
+
+set +e
+javac -cp $cpath *.java
+
+
+if [[ $? -eq 0 ]]
+then
+  echo "compiled"
+else
+  echo "didn't compiled"
+  exit 1
+fi
+
+java -cp $cpath org.junit.runner.JUnitCore TestListExamples > out.txt
+
+if [[ $? -eq 0 ]]
+then
+  grep "OK" out.txt
+  exit 0
+else
+  grep "Tests run:" out.txt
+  exit 1
+fi
+
+
+
+
+
